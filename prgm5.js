37a8b20eb99206ba1273a6591d0cd06c0dc9263e
Sneha G <sneha.g@v2solutions.com>
2022-09-19 15:38:12 +0530

    Updated prgm5.js

--- prgm5.js
+++ prgm5.js
@@ -32,3 +32,29 @@ function removeDuplicateChar(str) {
 }
 values= ['belle', 'candy', 'distributed', '113', 'Test' ,'T!esT!', ' money ', 'test apples', '$$%^*&','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse imperdiet metus et sodales dapibus. Fusce sodales in odio sit amet ornare. Proin suscipit elit at dui ornare molestie. Suspendisse congue finibus tellus, vel gravida lacus auctor non. Aliquam odio ex, venenatis vel pellentesque eu, cursus vel est. Etiam elementum ante odio, ac venenatis massa consequat ac. Pellentesque malesuada porta risus eget aliquam. Praesent suscipit ut justo tempus vehicula. Praesent hendrerit libero eu pharetra ornare. Sed nisi leo, laoreet vel dictum sed, finibus quis sem.'];
 removeDuplicate(values);
+
+// FOR TEST CASE:
+
+function removeDuplicates(str) {
+    var len = str.length,
+      char,
+      charCount = {},
+      newStr = [];
+    for (var i = 0; i < len; i++) {
+      char = str[i];
+      if (charCount[char]) {
+        charCount[char]++;
+      } else charCount[char] = 1;
+    }
+    for (var j in charCount) {
+      if (charCount[j] == 1) newStr.push(j);
+    }
+    return newStr.join("");
+  }
+ let values = ['belle', 'candy', 'distributed', '113', 'Test', 'T!esT!', ' money ', 'test apples', '$$%^*&','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse imperdiet metus et sodales dapibus. Fusce sodales in odio sit amet ornare. Proin suscipit elit at dui ornare molestie. Suspendisse congue finibus tellus, vel gravida lacus auctor non. Aliquam odio ex, venenatis vel pellentesque eu, cursus vel est. Etiam elementum ante odio, ac venenatis massa consequat ac. Pellentesque malesuada porta risus eget aliquam. Praesent suscipit ut justo tempus vehicula. Praesent hendrerit libero eu pharetra ornare. Sed nisi leo, laoreet vel dictum sed, finibus quis sem.'];
+
+ let expected = ['b','candy', 'srbue', '3', 'es', 'es', 'money',' al', '%^*&', 'xj'];
+
+ values.forEach((e,o)=>{removeDuplicates(e)!==expected[o]?(console.error("\n\n------- Failed -------"),console.error(`Test: ${e}`),console.error(`Result: ${removeDuplicates(e)}`),console.error(`Expected: ${expected[o]}`)):(console.log("\n\n------- Success -------"),console.log(`Test: ${e}`),console.log(`Result: ${removeDuplicates(e)}`))});
+
+
