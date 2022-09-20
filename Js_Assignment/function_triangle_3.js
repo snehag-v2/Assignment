4e500006c5385399db91fa7ef1cc3e7363cb3002
Sneha G <sneha.g@v2solutions.com>
2022-09-20 15:57:22 +0530

    updated function_triangle.js

--- Js_Assignment/function_triangle_3.js
+++ Js_Assignment/function_triangle_3.js
@@ -4,16 +4,13 @@ n = 5;
 string="";
 count=1;
 
-
 for ( i = 1; i <= n; i++) {
-
  for ( j = 1; j <= n - i; j++) {
    string += " ";
  }
 
  for ( k = 0; k < 2 * i - 1; k++) {
    string+=count;
-  
  }
  string+="\n";
 }
@@ -26,17 +23,12 @@ a=[];
 
   for ( i = 0; i < n; i++) {
        row = [1];
-     
       for ( j = 1; j < i; j++) {
-
-          row.push(a[i-1][j-1] + a[i-1][j])
-       
+          row.push(a[i-1][j-1] + a[i-1][j])       
       }
      
-      if( i > 0 ) row.push(1)
-     
-      a.push(row)
-     
+      if( i > 0 ) row.push(1)     
+      a.push(row)     
   }
   console.log(a);
 
@@ -45,16 +37,13 @@ a=[];
 n=7,num=1,k=n-1,string="";
 
 for(d=1;d<=n;d++){
-
     num=d;
     for(c=1;c<=k;c++)
     string += " ";
        k--;
    
     for(c=1;c<=d;c++){
-
      string +=num;
-
         num++;        
     }
     num--;
@@ -74,8 +63,7 @@ a= [10, 3,5, 12, 5, 3, 9, 1]
 a.sort();
  count = 1;
    results = "";
-  for ( i = 0; i < a.length; i++)
-  {
+  for ( i = 0; i < a.length; i++){
       if (a[i] == a[i+1])
       {
         count +=1;
