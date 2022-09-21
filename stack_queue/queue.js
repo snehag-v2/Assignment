197c426a08eb5dd4a29ce40ba074f541bc9b2f2d
Sneha G <sneha.g@v2solutions.com>
2022-09-21 18:58:39 +0530

    implemented using function and class

--- stack_queue/queue.js
+++ stack_queue/queue.js
@@ -1,3 +1,50 @@
+// USING FUNCTION
+
+function Queue1(){
+    let items = [];
+  
+    function add(element) {
+      items.push(element);
+      return items;
+    }
+  
+    function remove() {
+      return items.shift();
+    }
+  
+    function peek() {
+      return items[0];
+    }
+  
+    function isEmpty() {
+      return items.length === 0;
+    }
+  
+    function size() {
+      return items.length;
+    }
+    function printQueue(){
+        return items;
+    }
+    function clear() {
+      items = [];
+    }
+
+    return {add,remove,peek,isEmpty,size,clear,printQueue};
+  }
+
+let que1 = Queue1(20);
+que1.add("item 0");   // item 0
+que1.add("item 1");   // item 0, item 1
+que1.remove();    // item 0
+
+console.log(que1.size());
+console.log(que1.peek());
+console.log(que1.isEmpty());
+console.log(que1.printQueue());
+
+// USING CONSTRUCTOR
+
 class Queue{
     constructor(){
         this.items=[];
--- stack_queue/stack.js
+++ stack_queue/stack.js
@@ -1,5 +1,50 @@
 //Writing a program which implements a Stack and a Queue.  Both take the number of elements as the parameter.  Your program should take care of the edge cases. You cannot remove/pop from an empty Stack/Queue.  And you cannot add item to a Stack/Queue which is full. 
 
+//      USING FUNCTION
+
+    function Stack(){
+    let items = [];
+  
+    function push(element) {
+      items.push(element);
+    }
+  
+    function pop() {
+      return items.pop();
+    }
+  
+    function peek() {
+      return items[items.length - 1];
+    }
+  
+    function isEmpty() {
+      return items.length === 0;
+    }
+  
+    function size() {
+      return items.length;
+    }
+    function printStack(){
+        return items;
+    }
+    function clear() {
+      items = [];
+    }
+
+    return {push,pop,peek,isEmpty,size,clear,printStack};
+  }
+
+let stack1 = Stack(20);
+stack1.push("item 0");   // item 0
+stack1.push("item 1");   // item 0,item 1
+stack1.pop();            // item 1
+
+console.log(stack1.size());
+console.log(stack1.peek());
+console.log(stack1.isEmpty());
+console.log(stack1.printStack());
+
+// USING CONSTRUCTOR 
 
 class Stack{
 
